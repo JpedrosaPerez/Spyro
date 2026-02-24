4964e3804fd58186ca787320ff0b33096fd083f0
JpedrosaPerez <jpedrosaperez@gmail.com>
2026-02-24 19:50:35 +0100

    Segundo Dia(Tarde)

--- app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.kt
+++ app/src/main/java/dam/pmdm/spyrothedragon/MainActivity.kt
@@ -21,16 +21,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bindingGuide: GuideBinding
     private lateinit var bindingGuideStep: GuideStepBinding
     private var navController: NavController? = null
-
+    private var pasoActual = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        bindingGuide= GuideBinding.bind(binding.includeLayoutGuide.root)
-        bindingGuideStep= GuideStepBinding.bind(binding.includeLayoutGuideStep.root)
-        binding.includeLayoutGuide.root.visibility=View.VISIBLE
-        binding.includeLayoutGuideStep.root.visibility=View.GONE
+        bindingGuide = GuideBinding.bind(binding.includeLayoutGuide.root)
+        bindingGuideStep = GuideStepBinding.bind(binding.includeLayoutGuideStep.root)
+        binding.includeLayoutGuide.root.visibility = View.VISIBLE
+        binding.includeLayoutGuideStep.root.visibility = View.GONE
 
         val navHostFragment: Fragment? =
             supportFragmentManager.findFragmentById(R.id.navHostFragment)
@@ -53,6 +53,7 @@ class MainActivity : AppCompatActivity() {
                     // En las pantallas de los tabs no mostramos la flecha atrás
                     supportActionBar?.setDisplayHomeAsUpEnabled(false)
                 }
+
                 else -> {
                     // En el resto de pantallas sí
                     supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -60,10 +61,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         bindingGuide.buttonComenzar.setOnClickListener {
-
-            binding.includeLayoutGuide.root.visibility=View.GONE
-            binding.includeLayoutGuideStep.root.visibility=View.VISIBLE
-            step1()
+            startGuide()
         }
     }
 
@@ -71,8 +69,10 @@ class MainActivity : AppCompatActivity() {
         when (menuItem.itemId) {
             R.id.nav_characters ->
                 navController?.navigate(R.id.navigation_characters)
+
             R.id.nav_worlds ->
                 navController?.navigate(R.id.navigation_worlds)
+
             else ->
                 navController?.navigate(R.id.navigation_collectibles)
         }
@@ -100,39 +100,166 @@ class MainActivity : AppCompatActivity() {
             .setPositiveButton(R.string.accept, null)
             .show()
     }
-    private fun step1(){
-            val circulo=bindingGuideStep.circuloSelector
-            val texto=bindingGuideStep.textStep1
-            val nav=binding.navView
-            val primerItem=nav.width/3
-            circulo.animate()
-                .translationX(-primerItem.toFloat())
-                .translationY(200.toFloat())
-                .withEndAction { circulo.animate()
+
+
+    private fun startGuide() {
+        binding.includeLayoutGuide.root.visibility = View.GONE
+        binding.includeLayoutGuideStep.root.visibility = View.VISIBLE
+
+        step1()
+        pasoActual = 1
+
+    }
+
+    private fun step1() {
+        val circulo = bindingGuideStep.circuloSelector
+        val texto = bindingGuideStep.textStep1
+        val nav = binding.navView
+        val primerItem = nav.width / 3
+        circulo.animate()
+            .translationX(-primerItem.toFloat())
+            .translationY(200.toFloat())
+            .withEndAction {
+                circulo.animate()
                     .alpha(1f)
                 texto.animate()
-                    .alpha(1f)}
-                .start()
+                    .alpha(1f)
+            }
+            .start()
 
-        val scaleXCir= ObjectAnimator.ofFloat(circulo,View.SCALE_X, 1f,1.1f,1f).apply { duration=500
-        repeatCount=5}
-            val scaleYCir=ObjectAnimator.ofFloat(circulo,View.SCALE_Y,1f,1.1f,1f).apply { duration = 500
-            repeatCount=5}
-        val scaleXText= ObjectAnimator.ofFloat(texto,View.SCALE_X, 1f,1.1f,1f).apply { duration=500
-            repeatCount=5}
-        val scaleYText= ObjectAnimator.ofFloat(texto,View.SCALE_X, 1f,1.1f,1f).apply { duration=500
-            repeatCount=5}
-            scaleXCir.start()
-            scaleYCir.start()
+        val scaleXCir = ObjectAnimator.ofFloat(circulo, View.SCALE_X, 1f, 1.1f, 1f).apply {
+            duration = 500
+            repeatCount = 5
+        }
+        val scaleYCir = ObjectAnimator.ofFloat(circulo, View.SCALE_Y, 1f, 1.1f, 1f).apply {
+            duration = 500
+            repeatCount = 5
+        }
+        val scaleXText = ObjectAnimator.ofFloat(texto, View.SCALE_X, 1f, 1.1f, 1f).apply {
+            duration = 500
+            repeatCount = 5
+        }
+        val scaleYText = ObjectAnimator.ofFloat(texto, View.SCALE_X, 1f, 1.1f, 1f).apply {
+            duration = 500
+            repeatCount = 5
+        }
+        scaleXCir.start()
+        scaleYCir.start()
         scaleXText.start()
         scaleYText.start()
+        bindingGuideStep.root.setOnClickListener {
+            val fadeOutCirculo = ObjectAnimator.ofFloat(circulo, View.ALPHA, 1f, 0f).apply {
+                duration = 400
+            }
+            val fadeOutTexto = ObjectAnimator.ofFloat(texto, View.ALPHA, 1f, 0f).apply {
+                duration = 400
+            }
+            fadeOutTexto.start()
+            fadeOutCirculo.start()
 
+            step2()
+        }
+    }
 
+    private fun step2() {
+        val circulo = bindingGuideStep.circuloSelector
+        val texto = bindingGuideStep.textStep1
+        val nav = binding.navView
+        val primerItem = nav.width / 3
+        circulo.animate()
+            .translationXBy(primerItem.toFloat())
+            .translationY(200.toFloat())
+            .withEndAction {
+                circulo.animate()
+                    .alpha(1f)
+                texto.animate()
+                    .alpha(1f)
+            }
+            .start()
 
-
-
-
+        val scaleXCir = ObjectAnimator.ofFloat(circulo, View.SCALE_X, 1f, 1.1f, 1f).apply {
+            duration = 500
+            repeatCount = 5
+        }
+        val scaleYCir = ObjectAnimator.ofFloat(circulo, View.SCALE_Y, 1f, 1.1f, 1f).apply {
+            duration = 500
+            repeatCount = 5
+        }
+        val scaleXText = ObjectAnimator.ofFloat(texto, View.SCALE_X, 1f, 1.1f, 1f).apply {
+            duration = 500
+            repeatCount = 5
+        }
+        val scaleYText = ObjectAnimator.ofFloat(texto, View.SCALE_X, 1f, 1.1f, 1f).apply {
+            duration = 500
+            repeatCount = 5
+        }
+        scaleXCir.start()
+        scaleYCir.start()
+        scaleXText.start()
+        scaleYText.start()
+        bindingGuideStep.root.setOnClickListener {
+            val fadeOutCirculo = ObjectAnimator.ofFloat(circulo, View.ALPHA, 1f, 0f).apply {
+                duration = 400
+            }
+            val fadeOutTexto = ObjectAnimator.ofFloat(texto, View.ALPHA, 1f, 0f).apply {
+                duration = 400
+            }
+            fadeOutTexto.start()
+            fadeOutCirculo.start()
+            step3()
+        }
 
 
     }
-}
+
+    private fun step3() {
+
+            val circulo = bindingGuideStep.circuloSelector
+            val texto = bindingGuideStep.textStep1
+            val nav = binding.navView
+            val primerItem = nav.width / 3
+            circulo.animate()
+                .translationXBy(primerItem.toFloat())
+                .translationY(200.toFloat())
+                .withEndAction {
+                    circulo.animate()
+                        .alpha(1f)
+                    texto.animate()
+                        .alpha(1f)
+                }
+                .start()
+
+            val scaleXCir = ObjectAnimator.ofFloat(circulo, View.SCALE_X, 1f, 1.1f, 1f).apply {
+                duration = 500
+                repeatCount = 5
+            }
+            val scaleYCir = ObjectAnimator.ofFloat(circulo, View.SCALE_Y, 1f, 1.1f, 1f).apply {
+                duration = 500
+                repeatCount = 5
+            }
+            val scaleXText = ObjectAnimator.ofFloat(texto, View.SCALE_X, 1f, 1.1f, 1f).apply {
+                duration = 500
+                repeatCount = 5
+            }
+            val scaleYText = ObjectAnimator.ofFloat(texto, View.SCALE_X, 1f, 1.1f, 1f).apply {
+                duration = 500
+                repeatCount = 5
+            }
+            scaleXCir.start()
+            scaleYCir.start()
+            scaleXText.start()
+            scaleYText.start()
+            bindingGuideStep.root.setOnClickListener {
+                val fadeOutCirculo = ObjectAnimator.ofFloat(circulo, View.ALPHA, 1f, 0f).apply {
+                    duration = 400
+                }
+                val fadeOutTexto = ObjectAnimator.ofFloat(texto, View.ALPHA, 1f, 0f).apply {
+                    duration = 400
+                }
+                fadeOutTexto.start()
+                fadeOutCirculo.start()
+                step3()
+            }
+        }
+    }
+
